1e0a12026e6fca02b6cca1628ca2940a2e19d89a
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2024-11-22 08:53:51 +0000

    created Cookie object and imported lombok library

---
+++ src/main/model/Cookie.java
@@ -0,0 +1,18 @@
+package main.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cookie {
+    private String name;
+    private LocalDateTime accessDate;
+    
+}
